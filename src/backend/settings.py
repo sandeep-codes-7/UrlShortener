6719daff9355364238e7f20ad4841435fcc79218
Sandeep Karem <sandeepkarem.online@gmail.com>
2025-05-26 17:23:49 +0530

    updated

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv #type: ignore
 import os
 
 load_dotenv()
-print("sandeep",os.getenv("API"))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'short',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
--- src/short/models.py
+++ src/short/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import models #type: ignore
 
 # Create your models here.
+class UrlData(models.Model):
+    original_url = models.URLField(max_length=2000)
+    short_url = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.short_url
