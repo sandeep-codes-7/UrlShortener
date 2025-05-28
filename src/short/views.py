3480b0dfdb1ba4f78ea20427de6a683e024db092
Sandeep Karem <sandeepkarem.online@gmail.com>
2025-05-28 19:51:55 +0530

    updated

--- src/short/views.py
+++ src/short/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect #type: ignore
 from django.http import HttpResponse #type: ignore
 import random
-# Create your views here.
+from .models import UrlData #type: ignore
+
+
 def index(request):
     return render(request,'short/index.html')
 
@@ -10,15 +12,22 @@ def randomval():
     nums = [1,2,3,4,5,6,7,8,9,0]
     p = salphs+list(map(str,nums))
     random.shuffle(p)
-    random_pattern = random.choice(salphs)+random.choice(p)+random.choice(p)+random.choice(p)+random.choice(p)+random.choice(p)
+    random_pattern = random.choice(salphs)+random.choice(p)+random.choice(p)+random.choice(p)+random.choice(p)
     print(random_pattern)
     return random_pattern
 
-def generate(request):
+def result(request):
     if request.method == 'POST':
-        main_url = request.POST.get('url')
-        return render(request,'short/index.html',{'main_url':main_url})
-    return render(request,'short/index.html')
+        main_url = request.POST.get('main_url')
+        if not UrlData.objects.filter(original_url=main_url).exists():
+            UrlData.objects.create(original_url=main_url,short_url=randomval())
+            res = UrlData.objects.get(original_url=main_url).short_url
+        else:
+            res = UrlData.objects.get(original_url=main_url).short_url
+            return render(request,'short/error.html',{'result':res})
+        
+    return render(request,'short/result.html',{'result':res})
+
 
 
 
